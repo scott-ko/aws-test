d1e2f19f1d27af66398a112aca52629d7f55aa4f
Scott Ko <scott.ko@optus.com.au>
2017-11-27 10:57:48 +1100

    rename

--- cf/security-group-default-rule/run.sh
+++ cf/security-group-default-rule/run.sh
@@ -2,16 +2,15 @@
 
 aws cloudformation validate-template --template-body file://no-value-property-value.yaml
 
-aws cloudformation create-stack --stack-name security-group-default-rule --template-body file://no-value-property-value.yaml 
-sleep 10
+aws cloudformation deploy --stack-name security-group-default-rule --template-file no-value-property-value.yaml 
+
 awless list securitygroups
 
 aws cloudformation delete-stack --stack-name security-group-default-rule
 sleep 10
 
 
-aws cloudformation create-stack --stack-name security-group-default-rule --template-body file://no-value-property-value.yaml --parameters ParameterKey=Cidr1,ParameterValue=10.0.10.0/24
-sleep 10
+aws cloudformation deploy --stack-name security-group-default-rule --template-file no-value-property-value.yaml --parameter-overrides ParameterKey=Cidr1 ParameterValue=10.0.10.0/24
 awless list securitygroups
 
 aws cloudformation delete-stack --stack-name security-group-default-rule
