594b74b57494c345fa45761d2f8d7b022928a19d
Scott Ko <scott.ko@optus.com.au>
2017-12-08 18:59:43 +1100

    .

--- cf/cloud-watch-alert/send-alarm.sh
+++ cf/cloud-watch-alert/send-alarm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex
-aws cloudwatch put-metric-data --metric-name PeriodTest --namespace "Dev/Test1" --timestamp "$(date -u +''%FT%T.000Z'')" --value 1
+aws cloudwatch put-metric-data --metric-name LastResult --namespace "Dev/Test1" --timestamp "$(date -u +''%FT%T.000Z'')" --value 1
 
