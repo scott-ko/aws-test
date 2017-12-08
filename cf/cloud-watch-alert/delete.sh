162befc4c42d29ff76e0ba23f59cacb72e3d5a08
Scott Ko <scott.ko@optus.com.au>
2017-12-08 22:03:51 +1100

    Add dimensions

--- cf/cloud-watch-alert/delete.sh
+++ cf/cloud-watch-alert/delete.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+STACK=alarm-period-test
 aws cloudformation delete-stack --stack-name $STACK
--- cf/cloud-watch-alert/send-alarm.sh
+++ cf/cloud-watch-alert/send-alarm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex
-aws cloudwatch put-metric-data --metric-name LastResult --namespace "Dev/Test1" --timestamp "$(date -u +''%FT%T.000Z'')" --value 1
+aws cloudwatch put-metric-data --metric-name LastResult --dimensions Env=dev --namespace Test1 --timestamp "$(date -u +''%FT%T.000Z'')" --value 1
 
